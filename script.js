3232676779eaeee8f09b671479c09c03b46abdba
Patricia Correia <pncorreia25@gmail.com>
2022-04-25 10:36:10 -0300

    requisito 6 do projeto pixels art

---
+++ script.js
@@ -0,0 +1,7 @@
+/*
+ let seletor = document.getElementsByClassName('selected');
+function selecaoDeCor(){
+    let corInicial = 'black';
+    seletor.style.backgroundColor = corInicial;
+}
+    seletor.addEventListener('click', selecaoDeCor);*/
