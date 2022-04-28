cdd8f121bf572d5846468daa9b40f5e9ed6c0509
Patricia Correia <pncorreia25@gmail.com>
2022-04-28 10:53:40 -0300

    adiciona o requesito 8 do projeto pixels art

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
-function change(event){
-  
+function change(event){ 
     let selecionar = document.getElementsByClassName('selected')[0];
     selecionar.classList.remove('selected');
-    event.target.classList.add('selected');
-    
+    event.target.classList.add('selected');   
 }
     let changeSelector = document.querySelectorAll('.color');
     for (let i of changeSelector){
      i.addEventListener('click', change);
+    }
+// Busquei referências no w3schools e stackOverFlow para usar o getComputedStyle para manipular o estilo css do HTML. 
+function fixedColor(event){
+    const colors = document.querySelector('.selected');
+    const cssObj = window.getComputedStyle(colors, null).getPropertyValue("background-color");
+    event.target.style.backgroundColor = cssObj;
+    
 }
+    let paintPixels = document.querySelectorAll('.pixel');
+        for(let index of paintPixels){
+            index.addEventListener('click', fixedColor);
+            
+        }        
+        
+
+
+
